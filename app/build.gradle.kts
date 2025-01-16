92f7d2847922e0623d6b17374c5a1b9759ccd4b9
madhavkr1506 <madhavkr9153276724@gmail.com>
2025-01-16 18:45:31 +0530

    Updated Git Tutorial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.material.v190)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
